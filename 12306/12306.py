f3ad1841d4b0486c6b32fc81552a5c40ed09388f
zyy <zyywo1@yeah.net>
2019-02-10 13:43:01 +0800

    把发到站/乘车日期/座位类型/车次的设置从12306.py分离出来

--- 12306/12306.py
+++ 12306/12306.py
@@ -9,10 +9,12 @@ from selenium.webdriver.support.ui import Select
 from datetime import datetime
 from SendSMS import send_sms
 from setting import fromstation, tostation, date_of_leave, select_char, select_t
+import random
 import time
 
-#           ['商务座', '特等座', '一等座 ', '二等座', '高级软卧', '软卧', '动卧', '硬卧', '软座', '硬座', '无座', '其他']
+CHAIR_CN = ['商务座', '特等座', '一等座', '二等座', '高级软卧', '软卧', '动卧', '硬卧', '软座', '硬座', '无座', '其他']
 CHAR_TYPE = ['SWZ', 'TZ',  'ZY', 'ZE', 'GR', 'RW', 'SRRB', 'YW', 'RZ', 'YZ', 'WZ', 'QT']
+TICKET_IS_NONE = ['无', '--']
 login_url = 'https://kyfw.12306.cn/otn/resources/login.html'
 tick_url = 'https://kyfw.12306.cn/otn/leftTicket/init'
 order_url = 'https://kyfw.12306.cn/otn/confirmPassenger/initDc'
@@ -51,15 +53,13 @@ def get_trips():
     browser.find_element_by_id('date_icon_1').click()
     browser.find_element_by_xpath(get_date(date_of_leave)).click()
 
-    has_tickets = False
-    trips_info = {}
     pull = 0
-
     # 一直刷新，直到有票可订
-    while not has_tickets:
+    while True:
         print('{}  第{:05}轮：无票可抢，继续刷新中...'.format(datetime.now(), pull))
         pull += 1
-        trips_info.clear()
+        trains = None
+        time.sleep(random.randrange(1, 3))
 
         # 点击查询按钮
         query_btn = wait.until(expected_conditions.element_to_be_clickable((By.ID, 'query_ticket')))
@@ -68,14 +68,18 @@ def get_trips():
         except exceptions.ElementClickInterceptedException:
             print('查询按钮被其他元素遮挡，重试中...')
             continue
-        time.sleep(1)
+        finally:
+            time.sleep(1)
 
         try:
             tick_table = wait.until(expected_conditions.visibility_of_element_located((By.ID, 'queryLeftTable')))
             trains = tick_table.find_elements_by_tag_name('tr')[::2]
         except (exceptions.StaleElementReferenceException, exceptions.TimeoutException):
             print('查询失败， 重试中 ...')
+            time.sleep(2)
             continue
+        except Exception as e:
+            print(e)
 
         for t in trains:
             train_id = t.get_attribute('id').split('_')[1]
@@ -84,6 +88,9 @@ def get_trips():
             train_info = t.find_element_by_tag_name('div')
             # 获取车次
             trips = train_info.find_element_by_class_name('number').text
+
+            if trips not in select_t:
+                continue
 
             # 获取出发站\到达站
             ft_station = train_info.find_element_by_class_name('cdz').text
@@ -101,29 +108,20 @@ def get_trips():
                 ticket_num = [t.find_element_by_id(chairs_code[1]).text]
             ticket_num += [x for x in map(lambda x: t.find_element_by_id(x).text, chairs_code[2:])]
 
-            # 预定车票的按键
-            try:
-                order_btn = t.find_element_by_class_name('btn72')
-            except exceptions.NoSuchElementException:
-                order_btn = None
+            for _s_c in select_char:
+                # 该类型的座位有票
+                c_index = CHAIR_CN.index(_s_c)
+                if ticket_num[c_index-1 if c_index > 1 else 0] not in TICKET_IS_NONE:
+                    # 预定车票的按键
+                    order_btn = t.find_element_by_class_name('btn72')
+                    trips_info = [ft_station, lt_times, duration, ticket_num, order_btn]
 
-            # 预定按键可用，说明有票
-            if order_btn is not None:
-                if trips in select_t:
-                    has_tickets = True
-                    trips_info[trips] = [ft_station, lt_times, duration, ticket_num, order_btn]
+                    print('{}: {}'.format(trips, trips_info))
 
-        # 一个页面处理完后依然没有票的情况
-        if has_tickets is False:
-            continue
-
-        for i in trips_info:
-            print('{}: {}'.format(i, trips_info[i]))
-
-        return trips_info
+                    return trips_info, _s_c
 
 
-def order_ticket(target_trip):
+def order_ticket(target_trip, se_chair):
     # 点击预定
     target_trip[-1].click()
     wait.until(expected_conditions.url_to_be(order_url))
@@ -132,7 +130,6 @@ def order_ticket(target_trip):
     wait.until(expected_conditions.visibility_of_element_located((By.CLASS_NAME, 'per-sel')))
     per_sel = browser.find_element_by_class_name('per-sel')
     pers = per_sel.find_elements_by_tag_name('input')
-    print(pers)
     # print('乘客：', pers[0])
     # print('乘客姓名：', pers[0].text)
     pers[0].click()
@@ -144,29 +141,30 @@ def order_ticket(target_trip):
     seats = text.split()
     print('可选的座位类型：', seats)
     for s in seats:
-        if select_char in s:
+        if se_chair in s:
             print('选择的座位：', s)
             target_chair = s
             break
     try:
         Select(browser.find_element_by_id('seatType_1')).select_by_visible_text(target_chair)
     except Exception as e:
-        print(e)
+        print('line 153:   ', e)
 
     # 订票按钮
     btn = browser.find_element_by_id('submitOrder_id')
     btn.click()
-    # wait.until(expected_conditions.element_to_be_clickable((By.ID, 'qr_submit_id')))
-    sumbit = wait.until(expected_conditions.element_to_be_selected((By.ID, 'qr_submit_id')))
+    sumbit = wait.until(expected_conditions.element_to_be_clickable((By.ID, 'qr_submit_id')))
     wait.until(expected_conditions.element_to_be_clickable((By.ID, 'qr_submit_id')))
     print(sumbit.text)
     time.sleep(1)
     sumbit.click()
 
-    if wait.until(expected_conditions.url_changes(order_url)):
-        return True
-
-    return False
+    try:
+        if wait.until(expected_conditions.url_changes(order_url)):
+            return True
+    except exceptions.TimeoutException:
+        print('订单超时')
+        return False
 
 
 def get_date(_date):
@@ -189,9 +187,9 @@ if __name__ == '__main__':
 
     ok = False
     while not ok:
-        checi = get_trips()
+        checi, chair = get_trips()
         print('开始处理有票的情况...')
-        if not order_ticket(checi[select_t]):
+        if not order_ticket(checi, chair):
             print('订票失败，继续刷新中 ...')
         else:
             ok = True
--- 12306/setting.py
+++ 12306/setting.py
@@ -1,10 +1,11 @@
 # 出发站
 fromstation = '驻马店'
 # 到达站
-tostation = '虎门'
+tostation = '广州'
 # 乘车日期
-date_of_leave = '02-10'
+date_of_leave = '2-9'
 # 座位类型
-select_char = '二等座'
+select_char = ['二等座', '一等座']
 # 车次
-select_t = ['G71', ]
+select_t = ['G71', 'G531', 'G275', 'G821', 'G545', 'G75', 'G825', 'G839', 'G69', 'G843', 'G547']
+
